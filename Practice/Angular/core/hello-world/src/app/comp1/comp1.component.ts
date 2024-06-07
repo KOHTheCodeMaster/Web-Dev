395e075c96baf96ad20c72dba5c1f71eaf965f7e
Harshit Gupta <kohthecodemaster@gmail.com>
2024-06-07 13:30:15 +0530

    105th Commit - Angular Fundamentals - Introduction and Core Basics
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

---
+++ Practice/Angular/core/hello-world/src/app/comp1/comp1.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-comp1',   //  CSS selector
+  standalone: true,
+  imports: [],
+  templateUrl: './comp1.component.html',
+  styleUrls: ['./comp1.component.css']
+})
+export class Comp1Component {
+
+}
