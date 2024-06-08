0cadbd0c2e60425b9e01f84365a997df98cc51a5
Harshit Gupta <kohthecodemaster@gmail.com>
2024-06-08 13:02:47 +0530

    113th Commit - Angular Fundamentals - Data Binding
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- Practice/Angular/base/hello-world/src/app/app.component.ts
+++ Practice/Angular/base/hello-world/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Comp1Component } from './components/comp1/comp1.component';
 import { InlineTemplateComponent } from './components/inline-template/inline-template.component';
 import { CompSelectorDemoComponent } from './components/comp-selector/comp-selector-demo/comp-selector-demo.component';
+import { DataBindingComponent } from './components/data-binding/data-binding.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, Comp1Component, InlineTemplateComponent, CompSelectorDemoComponent],
+    imports: [RouterOutlet, Comp1Component, InlineTemplateComponent, CompSelectorDemoComponent, DataBindingComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
---
+++ Practice/Angular/base/hello-world/src/app/components/data-binding/data-binding.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-data-binding',
+  standalone: true,
+  imports: [],
+  templateUrl: './data-binding.component.html',
+  styleUrl: './data-binding.component.css'
+})
+export class DataBindingComponent {
+
+  name: string = "John Doe";
+  age: number = 17;
+  disableAgePlus: boolean = false;
+  disableAgeMinus: boolean = false;
+
+  isActive: boolean = true;
+  color: string = 'green';
+
+
+  incrementAge() {
+    this.age++;
+    if (this.age == 18) this.disableAgePlus = true;
+    if (this.age < 18) this.disableAgeMinus = false;
+  }
+
+  decrementAge() {
+    this.age--;
+    if (this.age == 13) this.disableAgeMinus = true;
+    if (this.age > 13) this.disableAgePlus = false;
+  }
+
+  getAge(): number {
+    return this.age;
+  }
+
+}
