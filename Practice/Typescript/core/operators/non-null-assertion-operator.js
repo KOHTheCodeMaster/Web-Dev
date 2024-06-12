607f898b8e6b37936a10cb007382dbaa423d2a40
Harshit Gupta <kohthecodemaster@gmail.com>
2024-06-13 01:18:50 +0530

    122nd Commit - Angular Fundamentals - Non-null Assertion Operator  - Updated Issue-Tracker.txt

---
+++ Practice/Typescript/core/operators/non-null-assertion-operator.js
@@ -0,0 +1,31 @@
+/*
+
+Date: 30th May 2K24 - 07:10 PM
+
+Title: Access Modifiers
+
+Dev: K.O.H..!! (Harshit Gupta)
+
+GitHub: https://github.com/KOHTheCodeMaster/Frontend-Web-Dev
+
+*/
+main();
+function main() {
+    demoNonNullAssertionOperator();
+    // demoNonNullAssertionOperator2();
+}
+function demoNonNullAssertionOperator() {
+    var username; // Declares username as string and asserts it's not nullish
+    function greetUser() {
+        console.log("Greetings " + username.toUpperCase()); // (No Compile-time Error)
+    }
+    username = "John Doe"; // Before calling greetUser, ensure username is properly initialized with a string value
+    greetUser();
+}
+function demoNonNullAssertionOperator2() {
+    var name = null; // Variable can be null
+    // Without Non-null Assertion Operator (No Compile-time Error)
+    console.log(name.toUpperCase());
+    // With Non-null Assertion Operator (!) (No Compile-time Error)
+    console.log(name.toUpperCase());
+}
