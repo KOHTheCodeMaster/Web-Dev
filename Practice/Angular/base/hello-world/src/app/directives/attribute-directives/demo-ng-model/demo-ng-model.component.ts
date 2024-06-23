82aa171da52f32897c9330ee294cb3e4203af4a7
Harshit Gupta <kohthecodemaster@gmail.com>
2024-06-23 15:40:03 +0530

    137th Commit - Angular Fundamentals - (ngModelChange) - Event Handling
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- Practice/Angular/base/hello-world/src/app/directives/attribute-directives/demo-ng-model/demo-ng-model.component.ts
+++ Practice/Angular/base/hello-world/src/app/directives/attribute-directives/demo-ng-model/demo-ng-model.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {FormsModule} from '@angular/forms';   // Import FormsModule
+import {FormsModule} from '@angular/forms'; // Import FormsModule
 
 @Component({
     selector: 'app-demo-ng-model',
@@ -9,5 +9,18 @@ import {FormsModule} from '@angular/forms';   // Import FormsModule
     styleUrl: './demo-ng-model.component.css'
 })
 export class DemoNgModelComponent {
-    name: string = 'John Doe';
+
+    name1: string = 'John Doe';
+    name2: string = 'John Doe';
+    age1: number = 1;
+
+    updateName2(name: any) {
+        console.log('name: ' + name + ' | ' + 'this.name2: ' + this.name2);
+        this.name2 = name + ' ! ';  //  Added ' ! ' text to visually see the difference.
+    }
+
+    public updateName2ToABC(): void {
+        this.name2 += 'ABC...';
+    }
+
 }
