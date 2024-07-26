f0a8d007b5d45a629dc7740f5cd7758e04258198
Harshit Gupta <kohthecodemaster@gmail.com>
2024-07-26 20:09:54 +0530

    #190 - Routing In Angular - [SMD] - Introduction to Angular Routing - Updated Demo-Intro-To-Routing Comprehensive Example
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- Practice/Angular/base/hello-world/src/app/components/routing/demo-intro-to-routing/demo-intro-to-routing.component.ts
+++ Practice/Angular/base/hello-world/src/app/components/routing/demo-intro-to-routing/demo-intro-to-routing.component.ts
@@ -1,12 +1,30 @@
 import {Component} from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
     selector: 'app-demo-intro-to-routing',
     standalone: true,
-    imports: [],
+    imports: [RouterOutlet, RouterLink],
     template: `
+        <br><hr><br>
         <h1>Introduction to Angular Routing</h1>
         <p>This is a demo component to introduce Angular routing.</p>
+        <br><hr><br>
+
+        <p>Below are the links to the components:</p>
+        <ul>
+            <li><a routerLink="first-comp">First Component</a></li>
+            <li><a routerLink="second-comp">Second Component</a></li>
+            <li><a routerLink="third-comp">Third Component</a></li>
+        </ul>
+        <br><hr><br>
+
+        <div>
+            <p>This place will be filled with the component content.</p>
+            <router-outlet></router-outlet>
+        </div>
+        <br><hr><br>
+
     `
 })
 export class DemoIntroToRoutingComponent {
