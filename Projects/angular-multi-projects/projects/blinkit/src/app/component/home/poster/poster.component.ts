fd6a924dbf9252bbb918a812c8f14922dff9fc05
Harshit Gupta <kohthecodemaster@gmail.com>
2025-01-02 08:23:24 +0530

    269 - Angular Projects - Blinkit - Poster Component Skeleton Design
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- Projects/angular-multi-projects/projects/blinkit/src/app/component/home/poster/poster.component.ts
+++ Projects/angular-multi-projects/projects/blinkit/src/app/component/home/poster/poster.component.ts
@@ -1,12 +1,34 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-poster',
-  standalone: true,
-  imports: [],
-  templateUrl: './poster.component.html',
-  styleUrl: './poster.component.css'
+    selector: 'app-poster',
+    standalone: true,
+    imports: [],
+    templateUrl: './poster.component.html',
+    styleUrl: './poster.component.css'
 })
 export class PosterComponent {
 
+    handlePosterClick(posterNumber: number) {
+
+        switch (posterNumber) {
+            case 1:
+                console.log('Poster 1 clicked');
+                break;
+            case 2:
+                console.log('Poster 2 clicked');
+                break;
+            case 3:
+                console.log('Poster 3 clicked');
+                break;
+            case 4:
+                console.log('Poster 4 clicked');
+                break;
+
+            default:
+                console.error('Poster not found');
+                break;
+        }
+
+    }
 }
