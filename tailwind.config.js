c300f47d8d83823a381a20c0eecdad84d2d31a1f
Harshit Gupta <kohthecodemaster@gmail.com>
2025-01-02 07:47:06 +0530

    268 - Angular Projects - Blinkit - Nav Bar Component Design Refinements - Assets Folder Configuration
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      "./src/**/*.{html,js}",
-      "./Practice/unsorted-projects/background-process-1/**/*.{html,js}",
+    "./src/**/*.{html,js}",
+    "./Practice/unsorted-projects/background-process-1/**/*.{html,js}",
   ],
   theme: {
     extend: {},
