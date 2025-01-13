828ef5771f760e431f5a1a0c6f5a8870e58cc046
Harshit Gupta <kohthecodemaster@gmail.com>
2025-01-13 20:49:00 +0530

    279 - Angular Projects - Removed Track It - Moving it to its own repository
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- Projects/angular-multi-projects/projects/track-it/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import {Routes} from '@angular/router';
-import {ManageLabelsComponent} from "./components/manage-labels/manage-labels.component";
-import {HomeComponent} from "./components/home/home.component";
-
-export const routes: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'manage-labels', component: ManageLabelsComponent},
-];
