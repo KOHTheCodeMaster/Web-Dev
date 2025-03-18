2d258bc0c8354edc5aa2ba8b92d99553fbf262c1
Harshit Gupta <kohthecodemaster@gmail.com>
2025-03-18 09:50:19 +0530

    285 - Angular Projects - Blinkit - Fixed Useful Links & Categories Component - Cosmetic Changes in Category List Bar & Shopping Component
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- Projects/angular-multi-projects/projects/blinkit/src/app/component/useful-links-and-categories/useful-links-and-categories.component.ts
+++ Projects/angular-multi-projects/projects/blinkit/src/app/component/useful-links-and-categories/useful-links-and-categories.component.ts
@@ -1,12 +1,44 @@
 import {Component} from '@angular/core';
+import {Category} from "../../shared/model/category.model";
+import {CategoryService} from "../../service/category.service";
+import {NgForOf} from "@angular/common";
+import {DataLoaderService} from "../../service/data-loader.service";
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-useful-links-and-categories',
-  standalone: true,
-  imports: [],
-  templateUrl: './useful-links-and-categories.component.html',
-  styleUrl: './useful-links-and-categories.component.css'
+    selector: 'app-useful-links-and-categories',
+    standalone: true,
+    imports: [
+        NgForOf
+    ],
+    templateUrl: './useful-links-and-categories.component.html',
+    styleUrl: './useful-links-and-categories.component.css'
 })
 export class UsefulLinksAndCategoriesComponent {
+
+    categoryList: Category[] = [];
+
+    constructor(private router: Router,
+                private dataLoaderService: DataLoaderService,
+                private categoryService: CategoryService) {
+
+        this.initSubscriptions();
+    }
+
+    private initSubscriptions() {
+
+        this.dataLoaderService.getDataLoaded$().subscribe(dataLoaded => {
+            if (dataLoaded) this.loadCategories();
+        });
+
+    }
+
+    private loadCategories() {
+        this.categoryList = this.categoryService.getCategoryList();
+    }
+
+    navigateToCategory(categoryId: number) {
+        this.router.navigate(['/shopping'], {queryParams: {categoryId: categoryId}});
+    }
 
 }
