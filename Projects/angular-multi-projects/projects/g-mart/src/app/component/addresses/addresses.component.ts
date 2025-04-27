2ebe96e89788d73ee63ee65bef7b0b6baf2684bc
Harshit Gupta <kohthecodemaster@gmail.com>
2025-04-27 23:54:21 +0530

    308 - G-Mart - Angular - Added Address Model along with Edit & Delete Popup Functionality
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- Projects/angular-multi-projects/projects/g-mart/src/app/component/addresses/addresses.component.ts
+++ Projects/angular-multi-projects/projects/g-mart/src/app/component/addresses/addresses.component.ts
@@ -1,19 +1,98 @@
-import {Component} from '@angular/core';
-import {Category} from "../../shared/model/category.model";
+import {Component, ElementRef, OnDestroy, Renderer2, ViewChild} from '@angular/core';
 import {Router, RouterLink} from "@angular/router";
+import {AsyncPipe, NgFor, NgForOf, NgIf} from '@angular/common';
+import {Address} from "../../shared/model/address.model";
 
 @Component({
     selector: 'app-addresses',
     standalone: true,
-    imports: [RouterLink],
+    imports: [RouterLink, NgIf, NgFor, NgForOf, AsyncPipe],
     templateUrl: './addresses.component.html'
 })
-export class AddressesComponent {
+export class AddressesComponent implements OnDestroy {
 
-    categoryList: Category[] = [];
+    addressList: Address[] = [];
+    private clickListener: (() => void) | undefined;
+    @ViewChild('deleteAddressPopup') deleteAddressPopup!: ElementRef;
 
-    constructor(private router: Router) {
+    constructor(private router: Router,
+                private renderer: Renderer2) {
 
+        this.initAddressList();
+        this.initSubscriptions();
+
+    }
+
+    private initAddressList() {
+
+        this.addressList = [];
+
+        this.addressList.push(new Address(
+            1,
+            "Mall",
+            'GT Central',
+            0,
+            "Malviya Nagar, Jaipur",
+            "WTP",
+            "John Doe",
+            1234567890,
+            ""));
+        this.addressList.push(new Address(
+            2,
+            "Work",
+            'Plot-001',
+            1,
+            "Mansarovar",
+            "Isckon Temple",
+            "Jane Doe",
+            9876543210,
+            ""));
+    }
+
+    initSubscriptions() {
+        //  Subscribe to address isDeleteConfirmationPopupOpened value changes
+        this.addressList.forEach(address => {
+            address.getIsDeleteConfirmationPopupOpened$().subscribe(isDeleteConfirmationPopupOpened => {
+                // Handle click outside logic
+                if (isDeleteConfirmationPopupOpened) {
+                    this.removeClickListener(); // Remove any existing listener
+                    this.clickListener = this.renderer.listen('document', 'click', (event: Event) => {
+                        const popupElement = this.deleteAddressPopup?.nativeElement;
+                        if (popupElement && !popupElement.contains(event.target)) {
+                            address.updateIsDeleteConfirmationPopupOpenedValue(false);
+                            this.removeClickListener();
+                        }
+                    });
+                } else {
+                    // If the popup is closed, remove the click listener to avoid memory leaks
+                    this.removeClickListener();
+                }
+            });
+        });
+    }
+
+
+    openEditAndDeletePopup(address: Address) {
+        // Close other popups
+        this.addressList
+            .filter(addressItem => addressItem !== address)
+            .forEach(addressItem => addressItem.updateIsEditAndDeletePopupOpenedValue(false));
+
+        // Toggle current popup
+        address.toggleEditAndDeletePopup();
+    }
+
+    removeClickListener() {
+        //  Remove the click listener if it exists to avoid memory leaks
+        if (this.clickListener) {
+            this.clickListener();
+            this.clickListener = undefined;
+        }
+    }
+
+    ngOnDestroy() {
+        //  Remove the click listener when the component is destroyed to avoid memory leaks
+        this.removeClickListener();
     }
 
 }
---
+++ Projects/angular-multi-projects/projects/g-mart/src/app/shared/model/address.model.ts
@@ -0,0 +1,119 @@
+import {BehaviorSubject, Observable} from "rxjs";
+
+export class Address {
+
+    private readonly id: number;
+    private label: string;
+    private houseNumberOrName: string;
+    private floor: number;
+    private locality: string;
+    private landmark: string;
+    private receiverName: string;
+    private mobNumber: number;
+    private googleMapsExtras: string;
+
+    private isEditAndDeletePopupOpened$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+    private isDeleteConfirmationPopupOpened$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+    constructor(id: number,
+                label: string,
+                houseNumberOrName: string,
+                floor: number,
+                locality: string,
+                landmark: string,
+                receiverName: string,
+                mobNumber: number,
+                googleMapsExtras: string) {
+        this.id = id;
+        this.label = label;
+        this.houseNumberOrName = houseNumberOrName;
+        this.floor = floor;
+        this.locality = locality;
+        this.landmark = landmark;
+        this.receiverName = receiverName;
+        this.mobNumber = mobNumber;
+        this.googleMapsExtras = googleMapsExtras;
+    }
+
+    toggleEditAndDeletePopup(): void {
+        const currentState = this.isEditAndDeletePopupOpened$.getValue();
+        this.updateIsEditAndDeletePopupOpenedValue(!currentState);
+
+        // this.isDeleteConfirmationPopupOpened &&= false;
+        this.updateIsDeleteConfirmationPopupOpenedValue(false);
+    }
+
+    toggleDeleteConfirmationPopup(): void {
+        const currentState = this.isDeleteConfirmationPopupOpened$.getValue();
+        this.isDeleteConfirmationPopupOpened$.next(!currentState);
+    }
+
+    handleDeleteAddress($event: MouseEvent) {
+        $event.stopPropagation();
+        this.toggleDeleteConfirmationPopup();
+    }
+
+    getFullAddress(): string {
+        return `${this.houseNumberOrName},
+                ${this.floor ? 'Floor ' + this.floor + ', ' : ''}${this.locality}, ${this.landmark}`;
+    }
+
+
+    //  Getters
+    //  -------
+
+    public getId(): number {
+        return this.id;
+    }
+
+    public getLabel(): string {
+        return this.label;
+    }
+
+    public getHouseNumber(): string {
+        return this.houseNumberOrName;
+    }
+
+    public getFloor(): number {
+        return this.floor;
+    }
+
+    public getLocality(): string {
+        return this.locality;
+    }
+
+    public getLandmark(): string {
+        return this.landmark;
+    }
+
+    public getReceiverName(): string {
+        return this.receiverName;
+    }
+
+    public getMobNumber(): number {
+        return this.mobNumber;
+    }
+
+    public getGoogleMapsExtras(): string {
+        return this.googleMapsExtras;
+    }
+
+    public getIsEditAndDeletePopupOpened$(): Observable<boolean> {
+        return this.isEditAndDeletePopupOpened$;
+    }
+
+    public updateIsEditAndDeletePopupOpenedValue(isEditAndDeletePopupOpened: boolean): void {
+        this.isEditAndDeletePopupOpened$.next(isEditAndDeletePopupOpened);
+    }
+
+    public getIsDeleteConfirmationPopupOpened$(): Observable<boolean> {
+        return this.isDeleteConfirmationPopupOpened$;
+    }
+
+    public updateIsDeleteConfirmationPopupOpenedValue(isDeleteConfirmationPopupOpened: boolean): void {
+        this.isDeleteConfirmationPopupOpened$.next(isDeleteConfirmationPopupOpened);
+    }
+
+
+
+}
