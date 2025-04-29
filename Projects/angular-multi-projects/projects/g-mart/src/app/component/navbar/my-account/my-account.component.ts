726dcd412c31eb5b1857516da69a94134be807e6
Harshit Gupta <kohthecodemaster@gmail.com>
2025-04-29 09:55:19 +0530

    309 - G-Mart - Angular - My Account Popup - Removed on Outside Click
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- Projects/angular-multi-projects/projects/g-mart/src/app/component/navbar/my-account/my-account.component.ts
+++ Projects/angular-multi-projects/projects/g-mart/src/app/component/navbar/my-account/my-account.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, Renderer2, ViewChild} from '@angular/core';
 import {Router} from "@angular/router";
 import {NgIf} from '@angular/common';
 
@@ -11,30 +11,62 @@ import {NgIf} from '@angular/common';
 export class MyAccountComponent {
 
     isMyAccountPopupOpened: boolean = false;
+    private clickListener: (() => void) | undefined;
+    @ViewChild('myAccountPopup') myAccountPopup!: ElementRef;
 
-    constructor(private router: Router) {
+    constructor(private router: Router,
+                private renderer: Renderer2) {
     }
 
     toggleMyAccountPopup() {
         this.isMyAccountPopupOpened = !this.isMyAccountPopupOpened;
+
+        // Handle click outside logic
+        if (this.isMyAccountPopupOpened) {
+            this.removeClickListener();
+            this.clickListener = this.renderer.listen('document', 'click', (event: Event) => {
+                const popupElement = this.myAccountPopup?.nativeElement;
+                if (popupElement && !popupElement.contains(event.target)) {
+                    this.updateMyAccountPopupFlag(false);
+                    this.removeClickListener();
+                }
+            });
+        } else {
+            // If the popup is closed, remove the click listener to avoid memory leaks
+            this.removeClickListener();
+        }
     }
 
-    closeMyAccountPopup() {
-        this.isMyAccountPopupOpened = false;
+    updateMyAccountPopupFlag(flag: boolean = false) {
+        this.isMyAccountPopupOpened = flag;
     }
 
     goToMyOrders() {
-        this.closeMyAccountPopup();
+        this.updateMyAccountPopupFlag(false);
         this.router.navigate(['/orders']);
     }
 
     goToSavedAddresses() {
-        this.closeMyAccountPopup();
+        this.updateMyAccountPopupFlag(false);
         this.router.navigate(['/addresses']);
     }
 
     logout() {
         console.log("Perform logout here...");
-        this.closeMyAccountPopup();
+        this.updateMyAccountPopupFlag(false);
     }
+
+    removeClickListener() {
+        //  Remove the click listener if it exists to avoid memory leaks
+        if (this.clickListener) {
+            this.clickListener();
+            this.clickListener = undefined;
+        }
+    }
+
+    ngOnDestroy() {
+        //  Remove the click listener when the component is destroyed to avoid memory leaks
+        this.removeClickListener();
+    }
+
 }
